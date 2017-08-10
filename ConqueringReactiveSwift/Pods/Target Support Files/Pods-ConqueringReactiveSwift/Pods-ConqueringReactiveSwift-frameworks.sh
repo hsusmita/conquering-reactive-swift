d9a51d2c609ba0adeae563011fa781b9fcdc2408
Susmita Horrow <hsusmita4@gmail.com>
2017-08-10 13:33:02 +0530

    chore(pod): add pod for ReactiveCocoa

--- ConqueringReactiveSwift/Pods/Target Support Files/Pods-ConqueringReactiveSwift/Pods-ConqueringReactiveSwift-frameworks.sh
+++ ConqueringReactiveSwift/Pods/Target Support Files/Pods-ConqueringReactiveSwift/Pods-ConqueringReactiveSwift-frameworks.sh
@@ -102,10 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveSwift/ReactiveSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveSwift/ReactiveSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
 fi
